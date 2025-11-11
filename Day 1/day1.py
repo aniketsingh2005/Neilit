cca0bc872b0867a6ebf74a75d684895f042440a6
aniketsingh2005 <aniketsingh61030@gmail.com>
2025-11-11 17:23:29 +0530

    first commit

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,24 +1,32 @@
-print("hello world")
+# print("hello world")
 
-#sum of two numbers:
-a = 5
-b = 2
-sum = a+b
-print(sum)
+# #sum of two numbers with dif,product,and division.
+# a = 5
+# b = 2
+# sum = a+b
+# print(sum)
 
-dif = a-b
-print(dif)
+# dif = a-b
+# print(dif)
 
-product = a*b
-print(product)
+# product = a*b
+# print(product)
 
-div = a/b
-print(div)
+# div = a/b
+# print(div)
 
-#find the area of the rectangle. Take the input from the user.a
+# #find the area of the rectangle. Take the input from the user.a
 
-len = int(input("Enter the length:"))
-br = int(input("Enter the breadth:"))
+# len = int(input("Enter the length:"))
+# br = int(input("Enter the breadth:"))
 
-Area = len*br
-print("The area of the rectangle is:",Area)
+# Area = len*br
+# print("The area of the rectangle is:",Area)
+
+# #Find the area of the circle1:
+
+
+
+radius = int(input("Enter the radius :"))
+area = 3.14 * radius * radius
+print("The area of the circle:",area)
