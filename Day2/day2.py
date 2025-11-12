39e94c708ccc8f853a44937667afb3b236817ecb
aniketsingh2005 <aniketsingh61030@gmail.com>
2025-11-12 17:27:33 +0530

    day2

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,1 +1,63 @@
-print("hello world")
+# age = int(input("Enter your age:"))
+
+# if age >= 18:
+#     print("Yes,You can vote.")
+
+# else:
+#     print("No,you can't vote.")
+
+# num = int(input("Enter your number:"))
+
+# if num%2 == 0:
+#     print("The number is even")
+# else:
+#     print("The number is odd")
+
+# Write a program to check if a number is positive or negative.
+
+# nums = int(input("Enter the number:"))
+
+# if nums == 0:
+#     print("0 is neither a positive nor a negative number")
+
+# elif nums < 0:
+#     print("The number is negative.")
+
+# else:
+#     print("The number is positive.")        
+
+
+# Write a program to check if a given number is divisible by 5.
+
+# a = int(input("Enter the number:"))
+
+# if a%5 == 0:
+#     print("The no. is divisible by 5")
+
+# else:
+#     print("The no. is not divisible by 5")          
+
+# Write a program to find the largest of two numbers
+
+# first = int(input("Enter the first no.:"))
+# second = int(input("Enter the second no.:"))
+
+# if first>second:
+#     print(first,"is greater than",second,".")
+
+# else:
+#     print(second,"is greater than",first,".")    
+
+
+num1 = int(input("Enter the first no.:"))
+num2 = int(input("Enter the second no.:"))
+num3 = int(input("Enter the third no.:"))
+
+if num1>num2 and num1>num3:
+    print(num1,"is greater than",num2,"and",num3)
+
+elif num2>num1 and num2>num3:
+    print(num2,"is greater than",num1,"and",num3)
+
+else:
+    print(num3,"is greater than",num1,"and",num2)        
