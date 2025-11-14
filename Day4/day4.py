a35ed055f64dd0bddedcb2056d169aab6799cc97
aniketsingh2005 <aniketsingh61030@gmail.com>
2025-11-14 17:17:46 +0530

    day4

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,1 +1,65 @@
-print("hello world")
+# Learning Def or Functions :
+# def add():
+#     a = 10
+#     b = 20
+#     c = a+b
+#     return(c)
+
+# print(add())
+
+# def fun():
+#     for i in range(5):
+#         print("Hello World")
+
+# fun()        
+
+# def greet():
+#     print("Hello, Welcome to Python!")
+
+# greet()    
+
+# def add(a,b):
+#     c = a+b
+#     print(c)
+
+# add(5,6)
+
+# def square(num):
+#     sqr = num**2
+#     print(sqr)
+
+# square(6)
+
+# def iseven(num):
+#     if num%2 == 0:
+#         print("True")
+#     else:
+#         print("False")
+
+# iseven(8)            
+
+# def greet_user(name) :
+#     print("Hello",name,"!")
+
+# greet_user("Kunal")    
+
+# def calculate_area(radius):
+#     print(3.14*radius**2)
+
+# calculate_area(2)    
+
+# def check_vowel(letter):
+#     if letter in "a,e,i,o,u":
+#         return("The given letter is vowel.")
+
+#     else:
+#         return("The given letter is consonant.")   
+
+# print(check_vowel("a"))
+
+
+def sum_list(numbers):
+    a = 0
+    
+
+        
